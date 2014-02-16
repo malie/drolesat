45dfd3a6c2a767e2d47f1bd89e0eaa4f84acc0ee
Markus Liedl <m.liedl@gmx.de>
2014-02-16 04:45:25 +0100

    solve cnf by enumerating models directly

--- RandomCNF.hs
+++ RandomCNF.hs
@@ -1,5 +1,5 @@
 module RandomCNF (
-  threeCNF , fourCNF
+  nCNF, threeCNF , fourCNF
   ) where
 
 import Control.Monad ( replicateM )
@@ -8,25 +8,20 @@ import Dimacs ( Dimacs, Literal )
 import Random ( randomInt , randomInts )
 
 threeCNF :: Int -> Int -> IO Dimacs
-threeCNF numVars numClauses =
-  replicateM numClauses (randomInts 3 1 numVars)
-  >>= mapM (mapM negateHalf)
-  where negateHalf x =
-          do a <- randomInt 0 1
-             return $ x * (2*a-1)
+threeCNF = nCNF 3
 
 fourCNF :: Int -> Int -> IO Dimacs
-fourCNF numVars numClauses =
-  replicateM numClauses (randomInts 4 1 numVars)
+fourCNF = nCNF 4
+
+nCNF :: Int -> Int -> Int -> IO Dimacs
+nCNF clauseSize numVars numClauses =
+  replicateM numClauses (randomInts clauseSize 1 numVars)
   >>= mapM (mapM negateHalf)
   where negateHalf x =
           do a <- randomInt 0 1
              return $ x * (2*a-1)
 
 
-
--- 2-3-n-cnf num vars, max clause size,
---           num clauses per size class
--- fixed-cutset-size-cnf
+-- randomly generate CNFs with a fixed cutset size?
 
   
---
+++ Testing.hs
@@ -0,0 +1,38 @@
+module Testing (main)
+       where
+
+import qualified Data.List as L
+import qualified Data.Set as S
+import Control.Monad ( foldM )
+import Data.Maybe ( catMaybes )
+
+import Dimacs ( Dimacs , Clause , Literal )
+import RandomCNF ( nCNF , threeCNF , fourCNF )
+
+type Model = S.Set Literal
+
+main = undefined
+
+-- Enumerate models directly from a cnf. No attempt is made
+-- at finding a good clause order.
+enumerateModels :: Dimacs -> [Model]
+enumerateModels = L.foldl' recur [S.empty]
+  where
+    recur xs clause =
+      catMaybes [ fuse x m | m <- clauseModels clause, x <- xs ]
+
+clauseModels :: Clause -> [Model]
+clauseModels clause =
+  L.delete (S.fromList $ map negate clause)
+     (L.foldl' ca [S.empty] clause)
+  where ca xs c =
+          let l = abs c
+          in concat [ [S.insert l x, S.insert (negate l) x]
+                    | x <- xs ]
+
+fuse :: Model -> Model -> Maybe Model
+fuse a b | S.size b > S.size a = fuse b a
+fuse a b = foldM recur a (S.toList b)
+  where recur a c
+          | S.member (negate c) a = Nothing
+          | otherwise             = Just $ S.insert c a
